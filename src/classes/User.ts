23ed7126d5c50244b7b73d5a9ab718e8eb58b8ab
messabih-khalil <msbih.khalil@gmail.com>
2023-04-04 06:53:52 +0100

    feat(User.ts): add info property to User class constructor

--- src/classes/User.ts
+++ src/classes/User.ts
@@ -4,12 +4,14 @@ import { Location } from '../intefaces/Location';
 export class User {
   name: string;
   location: Location;
-
+  info: string;
   constructor() {
     this.name = faker.name.firstName();
     this.location = {
       lat: parseFloat(faker.address.latitude()),
       lng: parseFloat(faker.address.longitude()),
     };
+
+    this.info = faker.lorem.paragraph();
   }
 }
